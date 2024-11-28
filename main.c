cf8ae965ffb9afe9e990884605cbeb81849fb06a
Abdellahsyani <syaniabdos@gmail.com>
2024-11-28 15:41:29 +0100

    add

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 int main() {
 	int fd;
 	char *line;
-	char *line1;
 
 
 
 	fd = open("text.txt", O_RDONLY); 
-	int fj = open("test.txt", O_RDONLY);
 	if (fd == -1) {
 		perror("Failed to open file");
 		return 1;
