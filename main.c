6f1eb924412eb909131df8383a14a917212e6beb
Abdellahsyani <syaniabdos@gmail.com>
2024-11-29 10:22:32 +0100

    add files

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 #include "get_next_line.h"
 
-int main() {
-	int fd;
+
+int main()
+{
 	char *line;
 
-
-
-	fd = open("text.txt", O_RDONLY); 
-	if (fd == -1) {
-		perror("Failed to open file");
-		return 1;
+	int fd = open("txt.txt", O_RDONLY);
+	if (fd == -1)
+		return -1;
+	while ((line = ft_get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
 	}
-	
-	 while ((line = ft_get_next_line(fd)) != NULL) {
-        	printf("%s", line);
-        	free(line);
-	}
-
-	close(fd);
-	return 0;
+	return (0);
 }
