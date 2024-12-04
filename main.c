056ab06731b8cabf54984469b348d7763e375c86
Abdellahsyani <syaniabdos@gmail.com>
2024-12-04 10:18:58 +0100

    updated

--- main.c
+++ main.c
@@ -4,25 +4,16 @@ int main()
 {
 	char *line;
 	int fd = open("file1", O_RDONLY); 
-	printf("------\n");
+	int fd1 = open("file2", O_RDONLY);
 	printf("-----\n");
 	while ((line = get_next_line(fd)) != NULL) {
 		printf("From file1: %s", line);
+		line = get_next_line(fd1);
+		printf("From file2: %s", line);
 		free(line);
 	}
-	//close(fd1);
-	if (BUFFER_SIZE > 100)
-	{
-		char *temp;
-		do {
-			temp = get_next_line(fd);
-			printf("%s", temp);
-			free(temp);
-		} while (temp != NULL);
-	}
-	/* 3 */ //test_gnl(fd, NULL);
-	//next_read_error = 0;
 	close(fd);
+	close(fd1);
 	return 0;
 }
 
